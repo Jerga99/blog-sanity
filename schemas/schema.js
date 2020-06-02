e7833877878e1d7788bfcdda41043afb75c3cc14
Filip Jerga <jerga99@gmail.comm>
2020-06-02 11:06:18 +0200

    8 Adds Hotspot

--- schemas/schema.js
+++ schemas/schema.js
@@ -47,6 +47,9 @@ export default createSchema({
           name: 'coverImage',
           title: 'Cover Image',
           type: 'image',
+          options: {
+            hotspot: true
+          },
           fields: [
             {
               type: 'text',
